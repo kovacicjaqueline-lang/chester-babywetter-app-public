5a5fe333c2d49d5564fd61e544442479e7b380e7
kovacicjaqueline-lang <316451114+kovacicjaqueline-lang@users.noreply.github.com>
2026-08-25 14:11:39 +0200

    feat: expose sleep bag recommendations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import { recommendOutfit as recommendBaseOutfit, TEMPERATURE_BANDS, temperatureBandFor } from './outfit-engine.js';
-import { applyGenericTogFallback, GENERIC_TOG_TABLE, genericTogGuidanceForRoomTemp } from './sleep-tog-rules.js';
+import {
+  applyGenericTogFallback,
+  GENERIC_TOG_TABLE,
+  genericTogGuidanceForRoomTemp,
+  sleepBagRecommendationFor
+} from './sleep-tog-rules.js';
 
 export { CLOTHING_CATALOG } from './clothing-catalog.js';
-export { TEMPERATURE_BANDS, temperatureBandFor, GENERIC_TOG_TABLE, genericTogGuidanceForRoomTemp };
+export {
+  TEMPERATURE_BANDS,
+  temperatureBandFor,
+  GENERIC_TOG_TABLE,
+  genericTogGuidanceForRoomTemp,
+  sleepBagRecommendationFor
+};
 
 export function recommendOutfit(input) {
   return applyGenericTogFallback(recommendBaseOutfit(input), input);
